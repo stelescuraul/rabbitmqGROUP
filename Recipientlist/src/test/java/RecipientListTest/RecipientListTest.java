63436ca4e16c9edf820196d076ef5f6935c64898
Zendikar <webdota@hotmail.com>
2016-12-03 14:56:30 +0100

    RecipientList added

---
+++ Recipientlist/src/test/java/RecipientListTest/RecipientListTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RecipientListTest;
+
+import com.groupx.recipientlist.test.RecipientList;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ * @author Tobias
+ */
+public class RecipientListTest {
+
+    public static void test() throws IOException, TimeoutException {
+//        RecipientList.main(new String[]{"kappa", "lol"});
+    }
+}
